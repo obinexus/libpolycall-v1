588f99c695aa3ae1afd0457261783bb9f38e6183
OBINexus TDA Agent <tda@obinexus.org>
2025-09-20 22:00:35 +0100

    core back fix

--- v2/include/libpolycall/core/polycall.h
+++ v2/include/libpolycall/core/polycall.h
@@ -1,32 +1,50 @@
-#ifndef POLYCALL_H
-#define POLYCALL_H
+#ifndef LIBPOLYCALL_CORE_POLYCALL_H
+#define LIBPOLYCALL_CORE_POLYCALL_H
 
-#include <pthread.h>
+#include <stddef.h>
+#include <stdint.h>
 
-typedef struct {
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-    int state;
-    int thread_count;
-    void* user_data;
-} polycall_context_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// Core functions
-int polycall_init(polycall_context_t* ctx);
-int polycall_process(polycall_context_t* ctx, int value);
-int polycall_wait(polycall_context_t* ctx, int target_state);
-int polycall_cleanup(polycall_context_t* ctx);
+/* Forward declaration for opaque pointer */
+struct polycall_context;
 
-// State machine
+/* Opaque context type - this is a POINTER */
+typedef struct polycall_context* polycall_context_t;
+
+/* Status codes */
 typedef enum {
-    STATE_INIT = 0,
-    STATE_READY,
-    STATE_PROCESSING,
-    STATE_COMPLETE,
-    STATE_ERROR
-} polycall_state_t;
+    POLYCALL_SUCCESS = 0,
+    POLYCALL_ERROR_INVALID_PARAMETERS = -1,
+    POLYCALL_ERROR_OUT_OF_MEMORY = -2,
+    POLYCALL_ERROR_NOT_INITIALIZED = -3,
+    POLYCALL_ERROR_ALREADY_INITIALIZED = -4,
+    POLYCALL_ERROR_INVALID_STATE = -5,
+    POLYCALL_ERROR_TIMEOUT = -6,
+    POLYCALL_ERROR_UNKNOWN = -99
+} polycall_status_t;
 
-const char* polycall_state_name(polycall_state_t state);
-int polycall_transition(polycall_state_t* current, polycall_state_t next);
+/* Configuration structure */
+typedef struct {
+    unsigned int flags;
+    size_t memory_pool_size;
+    void* user_data;
+} polycall_config_t;
 
-#endif // POLYCALL_H
+/* Core API functions */
+polycall_status_t polycall_init_with_config(
+    polycall_context_t* ctx, 
+    const polycall_config_t* config
+);
+
+void polycall_cleanup(polycall_context_t ctx);
+const char* polycall_get_version(void);
+const char* polycall_get_last_error(polycall_context_t ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBPOLYCALL_CORE_POLYCALL_H */
